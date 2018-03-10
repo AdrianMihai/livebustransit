9cbf69a6b2c49893cd8839a94cc2627c1a00e80f
AdrianMihai <adrian_cs98@yahoo.co.uk>
2018-03-10 15:47:20 +0200

    Add bus login with session

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express'),
 	fs = require('fs'),
 	url = require('url'),
 	mySql = require('mysql'),
+	bodyParser = require('body-parser'),
+	bcrypt = require('bcrypt'),
+	cookieSession = require('cookie-session'),
 	port = process.env.PORT || 3000;
 
 const io = require('socket.io')(server);
@@ -14,6 +17,7 @@ var con = mySql.createConnection({
   host: "sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "zjkmgixo5uc7x674",
   password: "q8g7itf4lyccen2a",
+  database: "f9q7qmaggj9xbvmq"
 });
 
 con.connect(function(err) {
@@ -21,9 +25,23 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
+
+//application resources folders setup
 app.use('/public', express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//cookie-session middleware
+app.use(cookieSession(
+	{
+		name: 'session',
+		keys: ['whatever'],
+		maxAge: 24 * 60 * 60 * 1000 
+	}
+));
 
 app.get('/', (req, res) => {
 	res.type('text/html');
@@ -38,6 +56,93 @@ app.get('/', (req, res) => {
 	
 });
 
+app.get('/busWatcher', (req, res) => {
+	res.type('text/html');
+
+	fs.readFile('views/busWatcher.html', (error, data) => {
+		if (error) {
+			throw error;
+		}
+
+		res.send(data);
+	});
+});
+
+app.get('/isBusLoggedIn', (request, response) => {
+
+	if (typeof request.session.busData === 'object') {
+		if (Object.keys(request.session.busData) == [])
+			request.session.busData = null;
+	}
+
+	response.type('json');
+
+	if (!request.session.busData) {
+		response.send(JSON.stringify({STATUS:  'NOT_LOGGED_IN' }));
+	}
+	else {
+		console.log(request.session.busData);
+
+		con.query(
+			"SELECT * FROM buses WHERE id=?",
+			[request.session.busData.id],
+			function(error, results, fields) {
+				console.log(results);
+
+				if (results) {
+					let busData = results[0];
+					busData['STATUS'] = 'LOGGED_IN';
+					busData['password'] = undefined;
+					response.send(JSON.stringify(busData));
+				}
+
+			}
+		);
+
+	}
+
+});
+
+app.post('/busLogIn', (req, response) => {
+	const saltRounds = 10;
+
+	response.type('json');
+
+  	con.query("SELECT * FROM buses WHERE username = ?", [req.body.username], function(error, results, fields){
+
+  		if (results.length === 0) {
+  			response.send(JSON.stringify({STATUS: "invalid_username"}));
+  		}
+
+  		else {
+  			let password = results[0].password;
+
+	  		bcrypt.compare(req.body.password, password, function(err, res) {
+
+	  			if (res === true) {
+	  				//insert data to session here
+	  				req.session.busData = {id: results[0]['id']};
+
+	  				results[0]['STATUS'] = "logged_in";
+	  				response.send(JSON.stringify(results[0]));
+	  			}
+	  			else {
+	  				response.send(JSON.stringify({STATUS: "invalid_password"}));
+	  			}
+	    		
+			});
+  		}
+  		
+  	});
+
+	/*
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  		res.send(hash);
+	});
+	*/
+});
+
+//sockets connections start here
 let userSockets = io.of('/'), usersCount = 0;
 
 userSockets.on('connection', function (socket) {
@@ -55,3 +160,5 @@ userSockets.on('connection', function (socket) {
     	console.log(usersCount);
     });
 });
+
+//con.end();
